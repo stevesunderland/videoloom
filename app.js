99760f2f8f8dd0e05b4cb7e1cbb540834da02605
Stephen <stevesunderland@gmail.com>
2015-04-18 13:43:15 -0700

    add map slash bio page

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ var timeout;
 var App = {
 	init: function(){
 		console.info('App.init');
-		App.showIntro();
+		// App.showIntro();
 		// App.openHaiku('landscape');
+		App.showBio();
 
 		$.each( $('.grid'), function(){
 			var row = $(this).data('row');
@@ -60,6 +61,13 @@ var App = {
 		console.info('App.showBio');
 
 		$('body').addClass('showBio animateLoom');
+
+		$('#bio .section').click(function(){
+			$('#bio .section').not(this).removeClass('active');
+			$(this).toggleClass('active');
+		});
+
+
 	},
 	loadClips: function(){
 		// var clips = [1,2,3,4,5,6,7,10,11,12,13,14,15,16,17,18,19,20,21,22,24,25];
